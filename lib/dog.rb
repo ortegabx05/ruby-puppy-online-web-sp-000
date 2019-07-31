854c41ae7df00c470f578e2ae26e44a1062f7d6e
jessi ortega <ortegabx05@gmail.com>
2019-07-31 20:40:49 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,23 @@
-# Add your code here
+class Dog 
+@@all = []
+attr_accessor :name 
+
+  def initialize(name)
+ @name = name 
+ @@all << self 
+  end
+
+  def self.all
+  @@all.each do |dog|
+    puts dog.name 
+    end 
+  end  
+
+  def self.clear_all 
+ @@all.clear
+  end 
+  
+  def self.print_all
+    @all.print.dog
+  end
+end 
